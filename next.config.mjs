8a9ced4c674bf6c49bd0769f1d7cd857c4586559
Hasal Dimantha <dimanthayapa2001@gmail.com>
2025-12-26 07:13:50 +0000

    images not apppear please check and fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -13,32 +11,22 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
        {
         protocol: 'https',
         hostname: 'plus.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
        {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
